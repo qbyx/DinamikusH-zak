3d2f603ccabc482c4d6d436dd6466a2249e623bc
qbyx <qbyx08@gmail.com>
2015-04-21 19:07:41 +0300

    First big commit.

---
+++ DinamikusHázak/src/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Szevasz Világ!");
+
+	}
+
+}
